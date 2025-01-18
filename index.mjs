9aee602a50b4918eeaf42c31024bef1d7ac396a9
jiukerZ <jx.zhu@foxmail.com>
2025-01-18 21:44:35 +0800

    feat

--- index.mjs
+++ index.mjs
@@ -10,7 +10,7 @@ export default {
     quotes: ['error',
       'single',
       { 'allowTemplateLiterals': true }
-    ], // 始终是单引号
+    ], 
     'object-curly-spacing': ['error', 'always'], // 对象的花括号头尾保持一个空格
     'object-curly-newline': ['error', { 'consistent': true }], // 对象元素换行保持一致
     'comma-dangle': ['error', 'never'], // 禁止尾随逗号
